5a33196febc9233f3427489515913af99d43e29f
jsmz <37253121+jsmzdf@users.noreply.github.com>
2020-03-07 21:57:01 -0500

    Update QUien pasa.pl
    se termina la consulta

--- QUien pasa.pl
+++ QUien pasa.pl
@@ -1,177 +1,64 @@
-pasa_asistenca(70).
 califica('parcial','quiz','tarea').
-peso('parcial',1,25).
-peso('parcial',2,25).
-peso('parcial',3,30).
-peso('quiz',1,5).
-peso('quiz',2,5).
-peso('tarea',1,5).
-peso('tarea',2,5).
+peso('parcial',0.25).
+peso('parcialf',0.30).
+peso('quiz',0.05).
+peso('tarea',0.05).
+pasa('pasa').
 tope_menor(0).
 tope_mayor(5).
 nota_pasa(3.0).
 pocerntaje_t_notas(100).
 asiste_PTQ('si','no').
-estudiante('mateo_nariño').
 materia('modelos2').
+estudiante('mateo_nariño').
 inscrito('mateo_nariño','modelos2').
-asiste('mateo_nariño','modelos2',80).
-presenta('mateo_nariño', 'parcial',1,'modelos2', 'si').
-presenta('mateo_nariño', 'parcial',2,'modelos2', 'si').
-presenta('mateo_nariño', 'parcial',3,'modelos2', 'si').
-presenta('mateo_nariño', 'quiz',1,'modelos2', 'si').
-presenta('mateo_nariño', 'quiz',2,'modelos2', 'si').
-presenta('mateo_nariño', 'tarea',1,'modelos2', 'si').
-presenta('mateo_nariño', 'tarea',2,'modelos2', 'si').
-nota('mateo_nariño','parcial',1,4,'modelos2').
-nota('mateo_nariño','parcial',2,4,'modelos2').
-nota('mateo_nariño','parcial',3,4,'modelos2').
-nota('mateo_nariño','quiz',1,4,'modelos2').
-nota('mateo_nariño','quiz',2,4,'modelos2').
-nota('mateo_nariño','tarea',1,4,'modelos2').
-nota('mateo_nariño','tarea',2,4,'modelos2').
+asiste('mateo_nariño','modelos2',70).
+nota1('mateo_nariño','parcial',1,'modelos2').
+nota2('mateo_nariño','parcial',1,'modelos2').
+nota3('mateo_nariño','parcialf',1,'modelos2').
+nota1('mateo_nariño','quiz',4,'modelos2').
+nota2('mateo_nariño','quiz',4,'modelos2').
+nota1('mateo_nariño','tarea',4,'modelos2').
+nota2('mateo_nariño','tarea',4,'modelos2').
 cancela('mateo_nariño', 'modelos2', 'no').
-inscrito_materia(A,B) :- inscrito(A,B),estudiante(A),materia(B).
 
 
-matriculado('sebastian_martinez','u distrital').
-rol('sebastian_martinez','estudiante').
-inscrito('Modelos2','sebastian_martinez').
-asiste('sebastian_martinez','modelos2',90).
-Presenta('sebastian_martinez', 'parcial',1,'modelos2', 'si').
-Presenta('sebastian_martinez', 'parcial',2,'modelos2', 'si').
-Presenta('sebastian_martinez', 'parcial',3,'modelos2', 'si').
-Presenta('sebastian_martinez', 'quiz',1,'modelos2', 'si').
-Presenta('sebastian_martinez', 'quiz',2,'modelos2', 'si').
-Presenta('sebastian_martinez', 'tarea',1,'modelos2', 'si').
-Presenta('sebastian_martinez', 'tarea',2,'modelos2', 'si').
-nota('sebastian_martinez','parcial',1,4,'modelos2').
-nota('sebastian_martinez','parcial',2,4,'modelos2').
-nota('sebastian_martinez','parcial',3,4,'modelos2').
-nota('sebastian_martinez','quiz',1,4,'modelos2').
-nota('sebastian_martinez','quiz',2,4,'modelos2').
-nota('sebastian_martinez','tarea',1,4,'modelos2').
-nota('sebastian_martinez','tarea',2,4,'modelos2').
-Cancela('sebastian_martinez', 'modelos2', 'no').
+estudiante('juan_mancera').
+inscrito('juan_mancera','modelos2').
+asiste('juan_mancera','modelos2',70).
+nota1('juan_mancera','parcial',4,'modelos2').
+nota2('juan_mancera','parcial',4,'modelos2').
+nota3('juan_mancera','parcialf',4,'modelos2').
+nota1('juan_mancera','quiz',4,'modelos2').
+nota2('juan_mancera','quiz',4,'modelos2').
+nota1('juan_mancera','tarea',4,'modelos2').
+nota2('juan_mancera','tarea',4,'modelos2').
+cancela('juan_mancera', 'modelos2', 'no').
+
+estudiante('sebastian_mrtinez').
+inscrito('sebastian_mrtinez','modelos2').
+asiste('sebastian_mrtinez','modelos2',70).
+nota1('sebastian_mrtinez','parcial',4,'modelos2').
+nota2('sebastian_mrtinez','parcial',4,'modelos2').
+nota3('sebastian_mrtinez','parcialf',4,'modelos2').
+nota1('sebastian_mrtinez','quiz',4,'modelos2').
+nota2('sebastian_mrtinez','quiz',4,'modelos2').
+nota1('sebastian_mrtinez','tarea',4,'modelos2').
+nota2('sebastian_mrtinez','tarea',4,'modelos2').
+cancela('sebastian_mrtinez', 'modelos2', 'no').
 
 
-matriculado('sebastian_mancera','u distrital').
-rol('sebastian_mancera','estudiante').
-inscrito('Modelos2','sebastian_mancera').
-asiste('sebastian_mancera','modelos2',70).
-Presenta('sebastian_mancera', 'parcial',1,'modelos2', 'no').
-Presenta('sebastian_mancera', 'parcial',2,'modelos2', 'si').
-Presenta('sebastian_mancera', 'parcial',3,'modelos2', 'si').
-Presenta('sebastian_mancera', 'quiz',1,'modelos2', 'si').
-Presenta('sebastian_mancera', 'quiz',2,'modelos2', 'si').
-Presenta('sebastian_mancera', 'tarea',1,'modelos2', 'no').
-Presenta('sebastian_mancera', 'tarea',2,'modelos2', 'si').
-nota('sebastian_mancera','parcial',1,0,'modelos2').
-nota('sebastian_mancera','parcial',2,4,'modelos2').
-nota('sebastian_mancera','parcial',3,4,'modelos2').
-nota('sebastian_mancera','quiz',1,4,'modelos2').
-nota('sebastian_mancera','quiz',2,4,'modelos2').
-nota('sebastian_mancera','tarea',1,0,'modelos2').
-nota('sebastian_mancera','tarea',2,4,'modelos2').
-Cancela('sebastian_mancera', 'modelos2', 'no').
-
-matriculado('cristia_benitez','u distrital').
-rol('cristia_benitez','estudiante').
-inscrito('Modelos2','cristia_benitez').
-asiste('cristia_benitez','modelos2',50).
-Presenta('cristia_benitez', 'parcial',1,'modelos2', 'si').
-Presenta('cristia_benitez', 'parcial',2,'modelos2', 'si').
-Presenta('cristia_benitez', 'parcial',3,'modelos2', 'no').
-Presenta('cristia_benitez', 'quiz',1,'modelos2', 'no').
-Presenta('cristia_benitez', 'quiz',2,'modelos2', 'no').
-Presenta('cristia_benitez', 'tarea',1,'modelos2', 'no').
-Presenta('cristia_benitez', 'tarea',2,'modelos2', 'no').
-nota('cristia_benitez','parcial',1,3,'modelos2').
-nota('cristia_benitez','parcial',2,2,'modelos2').
-nota('cristia_benitez','parcial',3,0,'modelos2').
-nota('cristia_benitez','quiz',1,0,'modelos2').
-nota('cristia_benitez','quiz',2,0,'modelos2').
-nota('cristia_benitez','tarea',1,0,'modelos2').
-nota('cristia_benitez','tarea',2,0,'modelos2').
-Cancela('cristia_benitez', 'modelos2', 'no').
-
-
-matriculado('juan_gaitan','u distrital').
-rol('juan_gaitan','estudiante').
-inscrito('Modelos2','juan_gaitan').
-asiste('juan_gaitan','modelos2',50).
-Presenta('juan_gaitan', 'parcial',1,'modelos2', 'si').
-Presenta('juan_gaitan', 'parcial',2,'modelos2', 'si').
-Presenta('juan_gaitan', 'parcial',3,'modelos2', 'no').
-Presenta('juan_gaitan', 'quiz',1,'modelos2', 'si').
-Presenta('juan_gaitan', 'quiz',2,'modelos2', 'si').
-Presenta('juan_gaitan', 'tarea',1,'modelos2', 'no').
-Presenta('juan_gaitan', 'tarea',2,'modelos2', 'no').
-nota('juan_gaitan','parcial',1,4,'modelos2').
-nota('juan_gaitan','parcial',2,4,'modelos2').
-nota('juan_gaitan','parcial',3,0,'modelos2').
-nota('juan_gaitan','quiz',1,4,'modelos2').
-nota('juan_gaitan','quiz',2,4,'modelos2').
-nota('juan_gaitan','tarea',1,0,'modelos2').
-nota('juan_gaitan','tarea',2,0,'modelos2').
-Cancela('juan_gaitan', 'modelos2', 'si').
-
-matriculado('john_zabala','u distrital').
-rol('john_zabala','estudiante').
-inscrito('Modelos2','john_zabala').
-asiste('john_zabala','modelos2',50).
-Presenta('john_zabala', 'parcial',1,'modelos2', 'si').
-Presenta('john_zabala', 'parcial',2,'modelos2', 'si').
-Presenta('john_zabala', 'parcial',3,'modelos2', 'no').
-Presenta('john_zabala', 'quiz',1,'modelos2', 'si').
-Presenta('john_zabala', 'quiz',2,'modelos2', 'si').
-Presenta('john_zabala', 'tarea',1,'modelos2', 'no').
-Presenta('john_zabala', 'tarea',2,'modelos2', 'no').
-nota('john_zabala','parcial',1,2,'modelos2').
-nota('john_zabala','parcial',2,3,'modelos2').
-nota('john_zabala','parcial',3,0,'modelos2').
-nota('john_zabala','quiz',1,1,'modelos2').
-nota('john_zabala','quiz',2,3.5,'modelos2').
-nota('john_zabala','tarea',1,0,'modelos2').
-nota('john_zabala','tarea',2,0,'modelos2').
-Cancela('john_zabala', 'modelos2', 'no').
-
-
-inscrito('Modelos2','alvaro_nariño').
-asiste('alvaro_nariño','modelos2',50).
-Presenta('alvaro_nariño', 'parcial',1,'modelos2', 'si').
-Presenta('alvaro_nariño', 'parcial',2,'modelos2', 'si').
-Presenta('alvaro_nariño', 'parcial',3,'modelos2', 'no').
-Presenta('alvaro_nariño', 'quiz',1,'modelos2', 'si').
-Presenta('alvaro_nariño', 'quiz',2,'modelos2', 'si').
-Presenta('alvaro_nariño', 'tarea',1,'modelos2', 'no').
-Presenta('alvaro_nariño', 'tarea',2,'modelos2', 'no').
-nota('alvaro_nariño','parcial',1,2,'modelos2').
-nota('alvaro_nariño','parcial',2,3,'modelos2').
-nota('alvaro_nariño','parcial',3,0,'modelos2').
-nota('alvaro_nariño','quiz',1,1,'modelos2').
-nota('alvaro_nariño','quiz',2,3.5,'modelos2').
-nota('alvaro_nariño','tarea',1,0,'modelos2').
-nota('alvaro_nariño','tarea',2,0,'modelos2').
-Cancela('alvaro_nariño', 'modelos2', 'no').
-
-
-inscrito('Modelos2','andres_mateo').
-asiste('andres_mateo','modelos2',50).
-Presenta('andres_mateo', 'parcial',1,'modelos2', 'si').
-Presenta('andres_mateo', 'parcial',2,'modelos2', 'si').
-Presenta('andres_mateo', 'parcial',3,'modelos2', 'no').
-Presenta('andres_mateo', 'quiz',1,'modelos2', 'si').
-Presenta('andres_mateo', 'quiz',2,'modelos2', 'si').
-Presenta('andres_mateo', 'tarea',1,'modelos2', 'no').
-Presenta('andres_mateo', 'tarea',2,'modelos2', 'no').
-nota('andres_mateo','parcial',1,2,'modelos2').
-nota('andres_mateo','parcial',2,3,'modelos2').
-nota('andres_mateo','parcial',3,0,'modelos2').
-nota('andres_mateo','quiz',1,1,'modelos2').
-nota('andres_mateo','quiz',2,3.5,'modelos2').
-nota('andres_mateo','tarea',1,0,'modelos2').
-nota('andres_mateo','tarea',2,0,'modelos2').
-Cancela('andres_mateo', 'modelos2', 'no').
-
-inscrito_materia(A,B) :- inscrito(A,B),estudiante(A),materia(B).
+inscri_mate(A,B) :- inscrito(A,B),estudiante(A),materia(B).
+pasa_asist(A,B,V) :- cancela(A,B,D),D =='no', inscri_mate(A,B),asiste(A,B,C),pasa(V),  C >= 70.
+not1p_peso(A,B,C) :-peso(Q,P),nota1(A,Q,N,B),Q=='parcial',C is (N*P).
+not2p_peso(A,B,C) :-peso(Q,P),nota2(A,Q,N,B),Q=='parcial',C is (N*P).
+not3p_peso(A,B,C) :-peso(Q,P),nota3(A,Q,N,B),Q=='parcialf',C is (N*P).
+parciales(A,B,Y) :- not1p_peso(A,B,C),not2p_peso(A,B,D),not3p_peso(A,B,E),Y is (C+D+E).
+not1q_peso(A,B,C) :-peso(Q,P),nota1(A,Q,N,B),Q=='quiz',C is (N*P).
+not2q_peso(A,B,C) :-peso(Q,P),nota2(A,Q,N,B),Q=='quiz',C is (N*P).
+quices(A,B,Y) :- not1q_peso(A,B,C),not2q_peso(A,B,D),Y is (C+D).
+not1t_peso(A,B,C) :-peso(Q,P),nota1(A,Q,N,B),Q=='tarea',C is (N*P).
+not2t_peso(A,B,C) :-peso(Q,P),nota2(A,Q,N,B),Q=='tarea',C is (N*P).
+tareas(A,B,Y) :- not1t_peso(A,B,C),not2t_peso(A,B,D),Y is (C+D).
+nota_total(A,B,Y) :- tareas(A,B,C),quices(A,B,D),parciales(A,B,E),Y is C+D+E.
+estudiantepasa(A,B,V):-nota_total(A,B,X),pasa_asist(A,B,V),X>3.
